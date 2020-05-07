acc47adfa343269a7a5f2c1a551f36bebc036a75
davidvang07 <davidvang07@mail.fresnostate.edu>
2020-05-06 21:54:06 -0700

    add test2

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -13,5 +13,7 @@ export class MainNavComponent implements OnInit {
 
   ngOnInit() {
   }
-
+showAbout() {
+this.toastService.showToast('success', 5000, 'This application is designed by David Vang. (C) 2018');
+}
 }
--- src/app/test-score/test-score.component.ts
+++ src/app/test-score/test-score.component.ts
@@ -4,6 +4,14 @@ import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 
+export interface ITest {
+  id?: number;
+  testName: string;
+  pointsPossible: number;
+  pointsReceived: number;
+  percentage: number;
+  grade: string;
+}
 
 @Component({
   selector: 'app-test-score',
@@ -12,16 +20,64 @@ import { ToastService } from '../toast/toast.service';
 })
 export class TestScoreComponent implements OnInit {
 
-  tests: Array<any> = [];
+  tests: Array<ITest> = [];
   constructor(
     private http: Http,
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private toastService: ToastService
-  ) { }
-
-  async ngOnInit() {
-
+  ) {
   }
 
+  async ngOnInit() {
+    const contacts = JSON.parse(localStorage.getItem('tests'));
+  if (this.tests && this.tests.length > 0) {
+    this.tests = this.tests;
+  } else {
+this.tests = await this.loadTestsFromJson();
+}
+console.log('this.tests from ngOninit...', this.tests);
+  }
+async loadTestsFromJson() {
+  const tests = await this.http.get('assets/tests.json').toPromise();
+  return tests.json();
+}
+addTest() {
+  const test: ITest = {
+    id: null,
+    testName: null,
+    pointsPossible: null,
+    pointsReceived: null,
+    percentage: null,
+    grade: null,
+  };
+  this.tests.unshift(test);
+  this.saveToLocalStorage();
+}
+deleteTest(index: number) {
+  this.tests.splice(index, 1);
+  this.saveToLocalStorage();
+}
+saveToLocalStorage() {
+  localStorage.setItem('tests', JSON.stringify(this.tests));
+}
+computeGrade() {
+  console.log('from computeGrade....');
+  const data = this.calculate();
+  this.router.navigate(['home', data]);
+}
+calculate() {
+  let grade = 0;
+  for (let i = 0; i < this.tests.length; i++) {
+// console.log('i--->', i "this.tests[i]", this.tests[i]);
+grade += this.tests[i].grade;
+// console.log('grade---->', grade);
+  }
+  return {
+    finalGrade: grade,
+    totalPointsPossible: grade,
+    totalPointsReceived: grade,
+    toalPercentage: (grade / grade)
+  };
+}
 }
